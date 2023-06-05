1d266a51bd74fbfdf3e431d190f1575111dbc608
andytestt <ipushok19@gmail.com>
2023-06-06 00:41:06 +0200

    Update get_birthdays.py

--- get_birthdays.py
+++ get_birthdays.py
@@ -7,29 +7,36 @@ def get_birthdays_per_week(users):
     end_of_week = start_of_week + timedelta(days=6)  # Кінець поточного тижня (неділя)
 
     birthdays_this_week = []
+    next_week = False
+
     for user in users:
         birthday = user['birthday'].date()
-        if start_of_week <= birthday <= end_of_week:
-            if birthday.weekday() >= 5:  # Якщо день народження - вихідний
+        birthday_this_year = datetime(datetime.now().year, birthday.month, birthday.day).date()
+
+        if (start_of_week <= birthday_this_year <= end_of_week) or (end_of_week.year < start_of_week.year and (start_of_week <= birthday_this_year or birthday_this_year <= end_of_week)):
+            if birthday_this_year.weekday() >= 5:  # Якщо день народження - вихідний
                 weekday = 'Monday'  # Привітати в понеділок
             else:
                 weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-                weekday = weekdays[birthday.weekday()]
+                weekday = weekdays[birthday_this_year.weekday()]
             birthdays_this_week.append((weekday, user['name']))
+        elif birthday_this_year > end_of_week and not next_week:
+            next_week = True
 
     if birthdays_this_week:
         print('Birthdays for this week:')
         for weekday, name in sorted(birthdays_this_week):
             print(f"{weekday}: {name}")
+    elif next_week:
+        print('No birthdays this week, but there are birthdays next week.')
     else:
-        print('No birthdays this week')
-
+        print('No birthdays this week.')
 
 users = [
-    {'name': 'Bill', 'birthday': datetime(2023, 5, 18)},
-    {'name': 'Jill', 'birthday': datetime(2023, 5, 20)},
-    {'name': 'Kim', 'birthday': datetime(2023, 5, 22)},
-    {'name': 'Jan', 'birthday': datetime(2023, 5, 23)}
+    {'name': 'Bill', 'birthday': datetime(1012, 6, 8)},
+    {'name': 'Jill', 'birthday': datetime(2013, 6, 10)},
+    {'name': 'Kim', 'birthday': datetime(2001, 6, 13)},
+    {'name': 'Jan', 'birthday': datetime(2010, 6, 4)}
 ]
 
 get_birthdays_per_week(users)
